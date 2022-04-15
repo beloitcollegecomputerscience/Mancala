1fdb8a29325142fce76e5068849ff44449a4d614
Auras Khanal <89553817+khanalauras@users.noreply.github.com>
2022-04-15 14:25:16 -0500

    Update Board.java

--- Mancala/Board.java
+++ Mancala/Board.java
@@ -1,5 +1,5 @@
-
 package Mancala;
+import java.util.Arrays;
 
 /** 
  * <p> An abstract class for implementing different types of boards </p>
@@ -13,29 +13,36 @@ package Mancala;
 
 public abstract class Board {
 	
-	int[][] board;
-	int totalSeeds;
+	int[] board;
+//	int totalSeeds;
 	
-	Board(int rows, int cols, int totalSeeds) {
-		board = new int[rows][cols];
-		this.totalSeeds = totalSeeds;
+	Board(int size, int seeds) {
+		board = new int[size];
+		Arrays.fill(board, seeds);
 	}
 	
-	abstract void setUpBoard();
+//	abstract void setUpBoard();
 	
 	void printBoard() {
-		for (int row = 0; row < board.length; row++) {
-			for (int col = 0; col < board[0].length; col++) {
-				System.out.print(board[row][col] + ",");
-			}
-			System.out.println();
-		}
+		for (int row=1; row < 3; row++ ){
+	          if (row==1){
+	              for (int i =0; i<board.length/2; i++){
+	                  System.out.print(board[i]+ ", ");
+	              }
+	          }
+	          else if (row==2){
+	              for (int i =6;i<board.length; i++){
+	                  System.out.print(board[i]+", ");
+	              }
+	          }
+	          System.out.println();
+	      }
 	}
 	
 	int getSeedCount(int row, int col) {
 		return board[row][col];
 	}
-	
+//	 it is altering the number of seeds in a particular hole
 	void setSeedCount(int row, int col, int num) {
 		board[row][col] = num;
 	}
