0304a5f605d3b7e6ac0e827737cfde5c131d8e89
Amir Camper <51269770+HappyCamper1v1@users.noreply.github.com>
2022-04-26 20:34:13 -0500

    Interface and Board
    The GUI team and Manual team made change to the board.
    The board is no longer an image.
    The board also provides a highlights when the circles are hovered over.

--- Main.java
+++ Main.java
@@ -6,18 +6,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/Interface.fxml"));
-			Scene scene = new Scene(root,600,400);
+			Scene scene = new Scene(root,1000,558);
 			Stage stage = new Stage();
 			stage.setResizable(false);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
